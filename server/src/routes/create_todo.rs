59aaeaa282981adccea530551b2fb02d9e68a65c
Sayan Mallick <snyxmk@gmail.com>
2022-01-30 20:46:01 +0530

    Added MongoDB support and gracefull shutdown

--- server/src/routes/create_todo.rs
+++ server/src/routes/create_todo.rs
@@ -1,6 +1,34 @@
-use poem::{handler, IntoResponse};
+use mongodb::{
+    bson::{doc, Document},
+    Collection,
+};
+use poem::{
+    handler,
+    web::{Data, Json},
+    IntoResponse,
+};
+
+use crate::todo::Todo;
 
 #[handler]
-pub fn create_todo() -> impl IntoResponse {
-    "New todo created".into_response()
+pub async fn create_todo(
+    Json(todo): Json<Todo>,
+    collection: Data<&Collection<Document>>,
+) -> impl IntoResponse {
+    println!("{:?}", todo);
+
+    let result = collection
+        .insert_one(
+            doc! {
+                "title": todo.title,
+                "completed": todo.completed,
+            },
+            None,
+        )
+        .await
+        .unwrap();
+
+    println!("{:?}", result);
+
+    "Created a new todo".into_response();
 }
--- server/src/routes/mod.rs
+++ server/src/routes/mod.rs
@@ -1,5 +1,7 @@
 mod create_todo;
+mod get_todos;
 mod ping;
 
 pub use create_todo::*;
+pub use get_todos::*;
 pub use ping::*;
